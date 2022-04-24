c934a25fefc08c3e8e6bda70c406eef46563dd83
Auguste Rame <auguste.rame@gmail.com>
2022-04-24 11:05:26 -0400

    Fix code structure

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 require("dotenv").config();
 
 const assert = require("assert");
-const axios = require("axios").default;
+const fatfs = require("fatfs");
+const utils = require("./utils");
 
 const SHEET_SIZE = 100 * 1000 * 3;
 const SECTOR_SIZE = 512;
-const ENDPOINT = process.env.ENDPOINT;
-
-async function get(from, to) {
-    const res = await axios.get(`${ENDPOINT}?from=${from}&to=${to}`);
-    if (res.status !== 200) throw "Ah shit (get)";
-    return (res).data;
-}
-
-async function set(from, data) {
-    const res = await axios.post(ENDPOINT, JSON.stringify({ from, data }), {});
-    if (res.status !== 200) throw "Ah shit (set)";
-    return;
-}
-
-async function randReadWrite(rounds) {
-    console.log(`Running random read-write test ${rounds}x`);
-
-    for (let i = 0; i < rounds; i++) {
-        console.time(`Round ${i + 1}`);
-        const from = Math.floor(Math.random() * SHEET_SIZE);
-        const size = Math.floor(Math.random() * 512);
-        const data = new Array(size).fill(0).map(_ => Math.floor(Math.random() * 255));
-
-        console.time(`Round ${i + 1} (set/${size})`);
-        await set(from, data);
-        console.timeEnd(`Round ${i + 1} (set/${size})`);
-
-        console.time(`Round ${i + 1} (get/${size})`);
-        const g = await get(from, from + size);
-        console.timeEnd(`Round ${i + 1} (get/${size})`);
-        
-        for (let i = 0; i < data.length; i++) {
-            if (g[i] !== data[i]) {
-                console.error(`Unexpected difference at ${i} (size ${size}): expected ${data[i]}, found ${g[i]}!`);
-            }
-        }
-        console.timeEnd(`Round ${i + 1}`);
-    }
-}
 
 (async () => {
-    // await randReadWrite(10);
-})();
+    console.log(`Creating file system with size ${SHEET_SIZE} and ${Math.floor(SHEET_SIZE / SECTOR_SIZE)} sectors of size ${SECTOR_SIZE}`);
 
+    // await set(0, [...utils.initFat16("DUMBFS".padEnd(11, " "))]);
+    const fs = fatfs.createFileSystem({
+        sectorSize: SECTOR_SIZE,
+        numSectors: Math.floor(SHEET_SIZE / SECTOR_SIZE),
+        async readSectors(i, dest, cb) {
+            console.log("Read!", dest.length);
+            if (dest.length % SECTOR_SIZE) throw Error("Unexpected buffer length!");
+
+            const buffer = Buffer.from(await utils.get(i * SECTOR_SIZE, i * SECTOR_SIZE + dest.length));
+            dest.set(buffer);
+            cb(null, buffer);
+            // fs.read(fd, dest, 0, dest.length, i*secSize, function (e,n,d) {
+            //     cb(e,d);
+            // });
+        },
+        async writeSectors(i, data, cb) {
+            console.log("Write!", i, data);
+            if (data.length % SECTOR_SIZE) throw Error("Unexpected buffer length!");
+            await utils.set(i * SECTOR_SIZE, [...data]);
+            cb(null);
+            // fs.write(fd, data, 0, data.length, i*secSize, function (e) {
+            //     cb(e);
+            // });
+        }
+    });
+
+    // console.log(fs);
+    // fs.mkdir("/abc", {}, () => {});
+    // fs.writeFile("/test.txt", "lorem ipsum", {}, err => {
+    //     if (err) throw err;
+    //     console.log("Wrote!");
+    // })
+    // fs.readdir("/", (err, files) => {
+    //     console.log(files);
+    // });
+    fs.readFile("/test.txt", {}, (err, data) => {
+        if (err) throw err;
+        console.log(err, data.toString());
+    });
+})();
